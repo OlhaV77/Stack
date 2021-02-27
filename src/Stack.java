d3148881436b656fb5d0ee06821543690d588427
olgaV77 <olga.v1977@gmail.com>
2021-02-27 18:25:01 -0500

    push and pop method reade

--- src/Stack.java
+++ src/Stack.java
@@ -16,11 +16,11 @@ class Run {
         System.out.println(stack.pop());    // Display: nothing on the stack...
 
 
-        stack.push(1);
-        stack.push(5);
-        stack.push(10);
-        stack.push(-1);
-        stack.push(5);
+        // stack.push(1);
+        // stack.push(5);
+        // stack.push(10);
+        // stack.push(-1);
+        // stack.push(5);
         //  stack.push(5);      // Display: can't add...
     }
 }
@@ -29,15 +29,30 @@ class Stack {
     int[] storage = new int[5];
     int count = 0;
 
+
     public void push(int num) {
         System.out.println("push -> " + num);
-        storage[count] = num;
+        storage[count] = num;                      //storege[0] = 1;
         count++;
+        for (int i = 0; i < count; i++) {     // storege[1] = 5
+            System.out.print("  " + storage[i]);
+        }
+        System.out.println();
+        System.out.println();
     }
 
 
+    int index = 0;
     public int pop() {
-        return 0;
+
+            for (int j = 0; j < count - index; j++) {
+                System.out.print( storage[ j] + "  ");
+            }
+        index++;
+        System.out.println();
+        System.out.print("pop  ");
+        return storage[count-index];
     }
+
 }
 
