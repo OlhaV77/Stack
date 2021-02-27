a3545cfa068a2fb793021a360ec137f46f626333
olgaV77 <olga.v1977@gmail.com>
2021-02-27 11:11:58 -0500

    push method this push number and stack number

--- src/Stack.java
+++ src/Stack.java
@@ -1,64 +1,43 @@
 
 class Run {
     public static void main(String[] args) {
-        //int[] pushArg = new int[]{56, 34, 1, 45, 7};
-        Stack[] stack = new Stack[5];
-       stack.toString();
+        Stack stack = new Stack();
 
+        stack.push(1);
+        stack.push(5);
+        stack.push(10);
+        stack.push(-1);
+
+
+        System.out.println(stack.pop());    // -1
+        System.out.println(stack.pop());    // 10
+        System.out.println(stack.pop());    // 5
+        System.out.println(stack.pop());    // 1
+        System.out.println(stack.pop());    // Display: nothing on the stack...
+
+
+        stack.push(1);
+        stack.push(5);
+        stack.push(10);
+        stack.push(-1);
+        stack.push(5);
+        //  stack.push(5);      // Display: can't add...
     }
 }
 
 class Stack {
-           int[] push;
+    int[] storage = new int[5];
+    int count = 0;
 
-           Stack(int[] pushArg){
-               push = pushArg;
+    public void push(int num) {
+        System.out.println("push -> " + num);
+        storage[count] = num;
+        count++;
+    }
 
-            }
 
-            public void push(int[] push) {
-
-            int index = 0;
-            for (int j = 0; j < 5; j++) {
-                index++;
-                if (push.length <= 5) {
-                    System.out.println("push -> " + push[j]);
-                }
-
-                if (push.length >= 6) {
-                    System.out.println("Can't add anymore");
-                }
-                System.out.print("stack -> ");
-                for (int i = 0; i < index; i++) {
-                    System.out.print(push[i] + "  ");
-                }
-                System.out.println();
-            }
-
-        }
-
-        public void pop(int[] pop) {
-            int index = 0;
-            System.out.println();
-            for (int i = pop.length - 1; i >= 0; i--) {
-                index++;
-                System.out.println("pop -> " + pop[i]);
-
-                if (pop.length == 0) {
-                    System.out.println("There is nothing on the stack");
-                }
-                System.out.print("stack -> ");
-                for (int j = 0; j < pop.length - index; j++) {
-                    System.out.print(pop[j] + " ");
-
-                }
-                System.out.println();
-            }
-        }
-
-    @Override
-    public String toString() {
-        return super.toString();
+    public int pop() {
+        return 0;
     }
 }
 
