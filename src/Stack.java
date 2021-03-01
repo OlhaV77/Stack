e587fa3fa887c3b3a81fcf572d5da13af7e69ea8
olgaV77 <olga.v1977@gmail.com>
2021-03-01 14:59:28 -0500

    2 methods and   check stack is empty

--- src/Stack.java
+++ src/Stack.java
@@ -13,15 +13,22 @@ class Run {
         System.out.println(stack.pop());    // 10
         System.out.println(stack.pop());    // 5
         System.out.println(stack.pop());    // 1
-        System.out.println(stack.pop());    // Display: nothing on the stack...
+        //  System.out.println(stack.pop());    // Display: nothing on the stack...
 
 
-        // stack.push(1);
-        // stack.push(5);
-        // stack.push(10);
-        // stack.push(-1);
-        // stack.push(5);
-        //  stack.push(5);      // Display: can't add...
+        stack.push(11);
+        stack.push(55);
+        stack.push(100);
+        stack.push(-11);
+        stack.push(55);
+        stack.push(5);
+        System.out.println(stack.pop());    // -11
+        System.out.println(stack.pop());    // 100
+        System.out.println(stack.pop());    // 55
+        System.out.println(stack.pop());    // 11
+
+        // Display: can't add...
+
     }
 }
 
@@ -29,30 +36,32 @@ class Stack {
     int[] storage = new int[5];
     int count = 0;
 
-
     public void push(int num) {
-        System.out.println("push -> " + num);
-        storage[count] = num;                      //storege[0] = 1;
-        count++;
-        for (int i = 0; i < count; i++) {     // storege[1] = 5
-            System.out.print("  " + storage[i]);
+        storage[count] = num;
+
+        if (count == 0) {
+            System.out.println("There is nothing on the stack");
+        }
+        count++;                   //1, 2, 3,4
+
+        for (int i = 0; i < count; i++) {
+            System.out.print(storage[i] + " ");
         }
         System.out.println();
-        System.out.println();
     }
 
-
-    int index = 0;
     public int pop() {
-
-            for (int j = 0; j < count - index; j++) {
-                System.out.print( storage[ j] + "  ");
+        count--;                 //3,2,1
+        for (int i = 0; i < count; i++) {
+            System.out.print(storage[i] + " ");
+            if (count > 5) {
+                System.out.println("Can't add anymore.");
             }
-        index++;
-        System.out.println();
-        System.out.print("pop  ");
-        return storage[count-index];
+        }
+        // return storage[count]
+        // count--;
+        return storage[count];
     }
+
 
 }
-
