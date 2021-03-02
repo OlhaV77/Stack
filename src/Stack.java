74eae35da383842231e6aef0783b5d2e0ed6f1d0
olgaV77 <olga.v1977@gmail.com>
2021-03-02 16:48:42 -0500

    change print stack with position 0

--- src/Stack.java
+++ src/Stack.java
@@ -2,55 +2,47 @@
 class Run {
     public static void main(String[] args) {
         Stack stack = new Stack();
-
         stack.push(1);
         stack.push(5);
         stack.push(10);
         stack.push(-1);
+        stack.push(-1);
 
+        Stack stack2 = new Stack();
+        stack2.push(1);
+        stack2.push(1);
+        stack2.push(1);
+        stack2.push(1);
+        stack2.push(1);
 
-        System.out.println(stack.pop());    // -1
-        System.out.println(stack.pop());    // 10
-        System.out.println(stack.pop());    // 5
-        System.out.println(stack.pop());    // 1
-        //  System.out.println(stack.pop());    // Display: nothing on the stack...
+        Stack[] stacks = new Stack[10]; // Fix this
+        stacks[0].push(1);
 
-
-        stack.push(11);
-        stack.push(55);
-        stack.push(100);
-        stack.push(-11);
-        stack.push(55);
-        stack.push(5);
-        System.out.println(stack.pop());    // -11
-        System.out.println(stack.pop());    // 100
-        System.out.println(stack.pop());    // 55
-        System.out.println(stack.pop());    // 11
-
-        // Display: can't add...
-
+        // TODO: encapsulate Stack class
     }
 }
 
 class Stack {
     int[] storage = new int[5];
-    int count = 0;
+    int position = 0;
 
     public void push(int num) {
-        if (count == 5) {
+        if (position == 5) {
             System.out.println("Can't  add anymore");
         } else {
-            storage[count] = num;
-            count++;
+            storage[position] = num;
+            position++;
         }
     }
 
     public int pop() {
-        count--;
-        if (count == 0) {
+        if (position < 1) {
             System.out.println("There is nothing on the stack");
+        } else {
+            position--;
         }
-        return storage[count];
+
+        return storage[position];
     }
 }
 
