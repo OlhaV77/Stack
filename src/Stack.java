8849308b7e88ec6a21da859032bd160e5fb32852
olgaV77 <olga.v1977@gmail.com>
2021-03-04 17:54:48 -0500

    class stack with method.

--- src/Stack.java
+++ src/Stack.java
@@ -1,3 +1,6 @@
+// Exceptions: checked vs unchecked
+
+
 public class Stack {
     private int[] storage = new int[5];
     private int position;
@@ -11,12 +14,14 @@ public class Stack {
         }
     }
 
+    // TODO: modify to throw an exception when empty stack
     public int pop() {
         if (position < 1) {
             System.out.println("There is nothing on the stack");
         } else {
             position--;
         }
+
         return storage[position];
     }
 }
