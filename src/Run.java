4de5749bdac5359237c779ade36beb966b7c794d
olgaV77 <olga.v1977@gmail.com>
2021-03-04 16:44:26 -0500

    2 diferent classes with access modifeir

---
+++ src/Run.java
@@ -0,0 +1,35 @@
+class Run {
+    public static void main(String[] args) {
+        Stack[] stacks = new Stack[10]; // Fix this
+        stacks[0] = new Stack();
+
+        stacks[0].push(2);
+        stacks[0].push(34);
+        stacks[0].push(35);
+        stacks[0].push(1);
+        stacks[0].push(8);
+        stacks[0].push(7);
+
+        stacks[0].pop();
+        stacks[0].pop();
+        stacks[0].pop();
+        stacks[0].pop();
+        stacks[0].pop();
+        stacks[0].pop();
+
+        stacks[0].push(90);
+        stacks[0].push(11);
+
+        stacks[1] = new Stack();
+        int[] storage = {0, 9, 8, 7, 78, 45, 34};
+        int count = 0;
+        for (int i = 0; i < storage.length; i++) {
+            count ++;
+            stacks[1].push(storage[i]);
+
+            if(count >=5){
+               stacks[1].pop();
+            }
+        }
+    }
+}
--- src/Stack.java
+++ src/Stack.java
@@ -1,69 +1,8 @@
-class Run {
-    @Override
-    public String toString() {
-        return super.toString();
-    }
-
-    public static void main(String[] args) {
-
-        Stack[] stacks = new Stack[10]; // Fix this
-        stacks[0] = new Stack();
-        stacks[0].setNumber(6);
-        stacks[0].push();
-        stacks[0].setNumber(78);
-        stacks[0].push();
-        stacks[0].setNumber(45);
-        stacks[0].push();
-        stacks[0].setNumber(3);
-        stacks[0].push();
-        stacks[0].setNumber(12);
-        stacks[0].push();
-        stacks[0].setNumber(67);
-        stacks[0].push();
-
-        stacks[0].pop();
-        stacks[0].pop();
-        stacks[0].pop();
-        stacks[0].pop();
-        stacks[0].pop();
-        stacks[0].pop();
-
-        stacks[0].setNumber(10);
-        stacks[0].push();
-        stacks[0].setNumber(11);
-        stacks[0].push();
-
-
-        stacks[1] = new Stack();
-        int[] storage = {0, 9, 8, 7, 78, 45, 34};
-        int count = 0;
-        for (int i = 0; i < storage.length; i++) {
-            count ++;
-            stacks[1].setNumber(storage[i]);
-            stacks[1].push();
-            if(count >=5){
-               stacks[1].pop();
-            }
-        }
-    }
-}
-
-
-// TODO: encapsulate Stack class
-
-
 public class Stack {
-
     private int[] storage = new int[5];
-    int position;
-    private int number;
+    private int position;
 
-
-    public void setNumber(int number) {                   //  encapsulation number
-        this.number = number;
-    }
-
-    public void push() {
+    public void push(int number) {
         if (position == 5) {
             System.out.println("Can't  add anymore");
         } else {
@@ -78,7 +17,7 @@ public class Stack {
         } else {
             position--;
         }
-        return number;
+        return storage[position];
     }
 }
 
