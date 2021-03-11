c94e242d5b7f306f24cec2f54d085f0bcff7a900
olgaV77 <olga.v1977@gmail.com>
2021-03-11 18:11:41 -0500

    aStack class with new size

---
+++ src/Astack.java
@@ -0,0 +1,28 @@
+public class Astack {
+    public static void main(String[] args) throws PushException {
+        Stack aStack = new Stack();
+        aStack = new Stack(5);
+        try {
+            aStack.push(1);
+            aStack.push(2);
+            aStack.push(5);
+            aStack.push(2);
+            aStack.push(9);
+            aStack.push(3);
+        } catch (PushException e) {
+            System.out.println(e.getMessage());
+        }
+
+        try {
+            aStack.pop();
+            aStack.pop();
+            aStack.pop();
+            aStack.pop();
+            aStack.pop();
+            aStack.pop();
+            aStack.pop();
+        } catch (PopException e) {
+            System.out.println(e.getMessage());
+        }
+    }
+}
